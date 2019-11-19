fc4b6b664780fa19bf845b73175e133cc706b598
George <gbakewell7@gmail.com>
2019-11-19 17:12:05 +0000

    Reduce fk.py to utility functions

--- src/fk.py
+++ src/fk.py
@@ -2,33 +2,7 @@
 
 import numpy as np
 
-last_link_length = 2
-
 # all coordinates are in homogenous coordinates
-
-# TODO
-def get_dh_parameters(rotations):
-    # for no rotations in robot
-    dh_parameters = [
-            {
-                "d": 2,
-                "theta": 0,
-                "r": 0,
-                "alpha": -np.pi/2,
-            },
-            {
-                "d": 0,
-                "theta": -np.pi/2,
-                "r": 0,
-                "alpha": -np.pi/2,
-            },
-            {
-                "d": 0,
-                "theta": 0,
-                "r": 3,
-                "alpha": np.pi/2,
-            },
-        ]
 
 def translate(dx, dy, dz):
     """ Return the transformation matrix for the translation through (dx, dy, dz)T """
@@ -54,10 +28,4 @@ def rotX(alpha):
 
 def dh_transformation(d, theta, r, alpha):
     return rotZ(theta) @ translate(r, 0, d) @ rotX(alpha)
-
-# link 0 is the yellow link (called link 1 in coursework document)
-def dh_transform(point, parameters):
-    for p in parameters:
-        point = dh_transformation(p['d'], p['theta'], p['r'], p['alpha']).dot(point)
-    return point
 
